bbd11829d39e438f44f465c24c891a5001b86b68
alexander.zelada <alexander.zelada@tmc.nl>
2023-03-14 18:22:12 +0100

    demo taller01

---
+++ taller02/networking/outputs.tf
@@ -0,0 +1,16 @@
+output "resource_group_name" {
+  value = data.azurerm_resource_group.rg.name
+}
+
+output "public_ip" {
+    value = azurerm_public_ip.public_ip.ip_address
+}
+
+output "virtual_machine_id" {
+  value = azurerm_virtual_machine.main.id
+}
+
+output "tls_private_key" {
+  value     = tls_private_key.example_ssh.private_key_pem
+  sensitive = true
+}
